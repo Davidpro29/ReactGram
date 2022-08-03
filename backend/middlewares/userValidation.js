adadb99c8ec1dd43118815eacd54a329cbe18f8f
David Robison <silva.estudo@hotmail.com>
2022-08-03 11:13:52 -0300

    validacoes com express-validate

--- backend/middlewares/userValidation.js
+++ backend/middlewares/userValidation.js
@@ -1,7 +1,25 @@
 const {body} = require("express-validator")
 
 const userCreateValidation = () =>{
-    return [body("name").isString().withMessage("O nome é obrigatório")];
+    return [
+    body("name")
+        .isString().withMessage("O nome é obrigatório.")
+        .isLength({min: 3}).withMessage("O nome precisa ter no mínimo 3 caracteres."),
+    body("email")
+        .isString().withMessage("O e-mail é obrigatório.")
+        .isEmail().withMessage("Insira um e-mail válido."),
+    body("password")    
+        .isString().withMessage("A senha é obrigatória.")
+        .isLength().withMessage("A senha precisa ter no mínimo 5 caracteres."),
+    body("confirmpassword")
+        .isString().withMessage("A confirmação de senha é obrigatória")
+        .custom((value, {req}) => {
+            if(value != req.body.password){
+                throw new Error("Senhas não conferem");
+            }
+            return true;
+        })
+];
 };
 
 module.exports = {
